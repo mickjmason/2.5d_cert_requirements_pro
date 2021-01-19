0e9fbc57ab8abfeb4c4846a4c03324f5111119e6
Mick Mason <mickjmason@outlook.com>
2021-01-19 05:44:18 +0000

    Pre-phase 1

---
+++ Assets/Scripts/LedgeGrabber.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgeGrabber : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _handPosition;
+
+    [SerializeField]
+    private Vector3 _standPosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("LedgeGrabChecker"))
+        {
+            var player = other.GetComponentInParent<Player>();
+            if(player == null)
+            {
+                Debug.LogError("Player is null");
+            }
+            player.GrabLedge(_handPosition, _standPosition);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    #region Inspector variables
+    [Header("Movement")]
+    [SerializeField]
+    private float _speed = 5f;
+
+    [SerializeField]
+    private float _jumpHeight = 20f;
+
+    [SerializeField]
+    private float _startingGravity = 1f;
+    #endregion
+
+    #region non-inspector private variables
+    private Vector3 _direction;
+    private CharacterController _characterController;
+    private Animator _animator;
+    private bool _jumping = true;
+    private float _currentGravity;
+    private bool _hanging = false;
+    private Vector3 _standPosition;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        LogNulls(_characterController);
+        _animator = GetComponentInChildren<Animator>();
+        LogNulls(_animator);
+        _currentGravity = _startingGravity;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessMovement();
+        ProcessHanging();
+    }
+
+    #region movement
+    private void ProcessMovement()
+    {
+        if (_characterController.enabled)
+        {
+
+            if (_characterController.isGrounded)
+            {
+                if (_jumping)
+                {
+                    _jumping = false;
+                    _animator.SetBool("IsJumping", false);
+
+                }
+                var directionInput = Input.GetAxisRaw("Horizontal");
+                _direction = new Vector3(0, 0, directionInput) * _speed;
+                _animator.SetFloat("Move", Mathf.Abs(directionInput));
+
+                if (directionInput != 0)
+                {
+                    Vector3 facing = transform.localEulerAngles;
+                    facing.y = _direction.z > 0 ? 0 : 180;
+                    transform.localEulerAngles = facing;
+                }
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+
+                    _direction.y += _jumpHeight;
+                    _jumping = true;
+                    _animator.SetBool("IsJumping", true);
+                }
+
+            }
+
+            _direction.y -= _currentGravity * Time.deltaTime;
+            _characterController.Move(_direction * Time.deltaTime);
+        }
+
+    }
+
+    private void ProcessHanging()
+    {
+        if (_hanging)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _animator.SetTrigger("LedgeClimb");
+                
+            }
+        }
+    }
+
+    public void StandUp() {
+
+        transform.position = _standPosition;
+        _animator.SetBool("Hanging", false);
+        _hanging = false;
+        _jumping = false;
+        _direction = Vector3.zero;
+        _characterController.enabled = true;
+    }
+
+    public void GrabLedge(Vector3 handPosition,Vector3 finalPosition)
+    {
+        _standPosition = finalPosition;
+        _hanging = true;
+        _characterController.enabled = false;
+        _animator.SetBool("Hanging", true);
+        transform.position = handPosition;
+        _jumping = false;
+        _animator.SetFloat("Move", 0.0f);
+        _animator.SetBool("IsJumping", false);
+    }
+    #endregion
+
+    #region environment interaction
+    #endregion
+
+
+    #region Coroutines
+    #endregion
+
+    #region util methods
+    private void LogNulls(object checkSource)
+    {
+        if(checkSource == null)
+        {
+            var name = checkSource.GetType().Name;
+            Debug.LogError(name + " is null");
+
+        }
+    }
+
+    private void SetCharacterDirection(float directionInput)
+    {
+        Vector3 facingDirection = transform.localEulerAngles;
+        
+        if(directionInput < 0)
+        {
+            facingDirection.y = 180;
+            
+        } else
+        {
+            facingDirection.y = 0;
+        }
+
+        transform.localEulerAngles = facingDirection;
+    }
+    #endregion
+}
