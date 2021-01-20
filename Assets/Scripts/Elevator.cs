ff67a47f3d3572499cc5e9c52fd1f78c3122ed04
Mick Mason <mickjmason@outlook.com>
2021-01-20 06:23:11 +0000

    Cinemachine Follow Cam:  Also added fix for elevator jitter.

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -13,6 +13,7 @@ public class Elevator : MonoBehaviour
 
 
     private int _currentTargetIndex;
+    private bool _canMove = true;
     void Start()
     {
         _currentTargetIndex = 0;
@@ -26,23 +27,38 @@ public class Elevator : MonoBehaviour
 
     void ProcessMovement()
     {
-        var distance = Vector3.Distance(transform.position, _waypoints[_currentTargetIndex].position);
-        if (distance > 1f)
+        if (_canMove)
         {
-            transform.position = Vector3.MoveTowards(transform.position, _waypoints[_currentTargetIndex].position, _speed * Time.deltaTime);
-        }
-        else
-        {
-            
-            StartCoroutine(SwitchWaypoints());
+            var distance = Vector3.Distance(transform.position, _waypoints[_currentTargetIndex].position);
+            if (distance > 0.01f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, _waypoints[_currentTargetIndex].position, _speed * Time.deltaTime);
+            }
+            else
+            {
+
+                StartCoroutine(SwitchWaypoints());
+            }
         }
 
     }
 
     IEnumerator SwitchWaypoints()
     {
+        _canMove = false;
         yield return new WaitForSeconds(5f);
         _currentTargetIndex = 1 - _currentTargetIndex;
+        float yAdjust;
+        if (_currentTargetIndex == 0)
+        {
+            yAdjust = 0.02f;
+        }
+        else
+        {
+            yAdjust = -0.02f;
+        }
+        transform.position += new Vector3(0f, yAdjust, 0f);
+        _canMove = true;
     }
 
 
